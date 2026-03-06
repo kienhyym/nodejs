4e0fdf2e5698333d9eda052c5c596414678692b4
Nguyen Dinh Kien <hydinhkien@gmail.com>
2026-03-06 23:53:16 +0700

    bo auth

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = require('express/lib/router');
 const { CreateUser, handleLogin, getUser, getAccount } = require('../controllers/userController');
-const auth = require('../middleware/auth');
+// const auth = require('../middleware/auth');
 
 const routerAPI = express.Router();
 
-routerAPI.all('*', auth)
+// routerAPI.all('*', auth)
 routerAPI.get('/',  (req, res) => {
     return res.status(200).json({ message: 'Hello World API' });
 })
