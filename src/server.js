836de64cd1ed4d5501046e9dee1375c4fa94d225
Nguyen Dinh Kien <hydinhkien@gmail.com>
2026-04-10 22:36:26 +0700

    update

--- src/server.js
+++ src/server.js
@@ -15,7 +15,18 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://admin-hoc8.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
 
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.options('*', cors()); // 👈 bắt buộc
 
 // app.use(delay);
